620b96d00a37bcb6a1f13822684d8b473a00d36f
Ayushupadhyay14 <Ayushupadhyay88151528@gmail.com>
2025-06-24 22:39:14 +0530

    main branch code

--- ch18/ch18/settings.py
+++ ch18/ch18/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+"""Default set cache in program"""
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHE_MIDDLEWARE_SECONDS = 30
 
