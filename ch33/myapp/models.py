1383ef3c1d2ccf93aed81e1f4681ca4a36928fad
Ayushupadhyay14 <Ayushupadhyay88151528@gmail.com>
2025-07-19 03:57:34 +0530

    Add Read in project ch32

--- ch33/myapp/models.py
+++ ch33/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+
+from django.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    msg = models.TextField()
+
+    def __str__(self):
+        return self.name
--- ch33/myapp/views.py
+++ ch33/myapp/views.py
@@ -2,15 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from myapp.forms import ContactForm
+from .models import Contact
+
 # Create your views here.
 
 
 class ContactFormView(FormView):
+
     template_name = 'myapp/contact.html'
     form_class = ContactForm
 
     def form_valid(self, form):
+        
         print(form.cleaned_data['name'])
         print(form.cleaned_data['email'])
         print(form.cleaned_data['msg'])
+
         return HttpResponse('thank You form submitted')
