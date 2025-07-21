7b56fe283876b895e37899266faa022967ad37d3
Ayushupadhyay14 <Ayushupadhyay88151528@gmail.com>
2025-07-21 22:30:20 +0530

    CRUD Project using Generic Class based View in Django 5

---
+++ CRUDProGVC/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from myapp.models import Student  # here import studemt models
+from django.views.generic import ListView, DateDetailView, CreateView, DeleteView, UpdateView
+# HERE INMPORT (GENERIC LIBARY CLSS IMPORT VIEWS)
+# Create your views here.
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ['name', 'email', 'roll']
+    success_url = '/'
