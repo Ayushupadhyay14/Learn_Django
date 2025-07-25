b5ffa9ad640b467b2c35f48acddc52d50fd25e2c
Ayushupadhyay14 <Ayushupadhyay88151528@gmail.com>
2025-07-26 00:51:34 +0530

    add some code

--- ch35/ch35/myapp/views.py
+++ ch35/ch35/myapp/views.py
@@ -1,15 +1,35 @@
 # from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.http import JsonResponse
-
+from myapp.models import Student
+from django.core.serializers import serialize
 # Create your views here.
 
 
 # def home_view(request):
 #     return HttpResponse("<h1>this is a home page</h1>")
+# def home_view(request):
+#     data = {
+#         'massage': "home page",
+#         'status': 'success'
+#     }
+#     return JsonResponse(data)
+"""------HERE CHANGE STATUS CODE ------"""
+
+
+# def home_view(request):
+#     data = {
+#         'massage': "home page",
+#         'status': 'success'
+#     }
+#     return JsonResponse(data, status=200)
+
+"""HERE RETURN A Queryset data"""
+# def home_view(request):
+#     data = ['Ayush', 'raj', 'sonam']
+#     return JsonResponse(data, safe=False)
+
+
 def home_view(request):
-    data = {
-        'massage': "home page",
-        'status': 'success'
-    }
-    return JsonResponse(data)
+    data = serialize('json', Student.objects.all())
+    return JsonResponse(data, status=200, safe=False)
